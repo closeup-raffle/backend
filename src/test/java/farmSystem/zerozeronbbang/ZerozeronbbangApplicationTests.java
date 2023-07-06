1d1fc59852bb7d10648d82833103ecfdce79c948
thisishwan2 <feel000617@gmail.com>
2023-07-07 01:40:43 +0900

    [FEAT] init Setting

---
+++ src/test/java/farmSystem/zerozeronbbang/ZerozeronbbangApplicationTests.java
@@ -0,0 +1,13 @@
+package farmSystem.zerozeronbbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerozeronbbangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
