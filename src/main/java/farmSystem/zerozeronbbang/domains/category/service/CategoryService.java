d2c452d09839728604a5e33f893ae6c6e061bfd6
thisishwan2 <feel000617@gmail.com>
2023-07-17 16:10:50 +0900

    [FEAT] CategoryService 생성

---
+++ src/main/java/farmSystem/zerozeronbbang/domains/category/service/CategoryService.java
@@ -0,0 +1,10 @@
+package farmSystem.zerozeronbbang.domains.category.service;
+
+import farmSystem.zerozeronbbang.domains.category.dto.ResFindCategoriseDto;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<ResFindCategoriseDto> findCategories();
+}
