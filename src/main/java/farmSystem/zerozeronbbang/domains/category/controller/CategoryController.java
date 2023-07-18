d75c6fb1376d705fdfaf4fae37335813d43ac648
thisishwan2 <feel000617@gmail.com>
2023-07-19 02:32:19 +0900

    [FIX] URL 재정의

--- src/main/java/farmSystem/zerozeronbbang/domains/category/controller/CategoryController.java
+++ src/main/java/farmSystem/zerozeronbbang/domains/category/controller/CategoryController.java
@@ -15,17 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Tag(name = "Category", description = "카테고리 선택 api")
+@Tag(name = "Category", description = "카테고리 관련 api")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/category")
 public class CategoryController {
 
     private final CategoryServiceImpl categoryService;
 
     @Operation(summary = "category select", description = "메인 페이지 category select 컨트롤러")
-    @GetMapping(value = "/")
-    public ResponseDto<?> getCategories(@AuthenticationPrincipal final String email){
+    @GetMapping(value = "/categories")
+    public ResponseDto<?> findCategories(@AuthenticationPrincipal final String email){
         List<ResFindCategoriseDto> categories = categoryService.findCategories();
         return ResponseUtil.SUCCESS(ResCodeEnum.CATEGORY_LOAD_SUCCESS.getMessage(), categories);
     }
