da2a09ca1c8a79b3a09fbbdc94eeac9625a5ed8d
thisishwan2 <feel000617@gmail.com>
2023-07-21 03:30:35 +0900

    [FIX] 응답 처리 변경

--- src/main/java/farmSystem/zerozeronbbang/domains/user/service/UserService.java
+++ src/main/java/farmSystem/zerozeronbbang/domains/user/service/UserService.java
@@ -1,13 +1,15 @@
 package farmSystem.zerozeronbbang.domains.user.service;
 
-import farmSystem.zerozeronbbang.domains.user.User;
+import farmSystem.zerozeronbbang.domains.user.dto.ReqLoginDto;
 import farmSystem.zerozeronbbang.domains.user.dto.ReqSignUpDto;
 import farmSystem.zerozeronbbang.domains.user.dto.ResLoginDto;
-
-import java.util.Optional;
+import farmSystem.zerozeronbbang.domains.user.dto.ResSignUpDto;
 
 public interface UserService {
-    ResLoginDto login(String email, String password);
-    ReqSignUpDto signUp(ReqSignUpDto reqSignUpDto);
+//    ResLoginDto login(String email, String password);
+
+    ResLoginDto login(ReqLoginDto reqLoginDto);
+
+    ResSignUpDto signUp(ReqSignUpDto reqSignUpDto);
     String findUser(String email);
 }
