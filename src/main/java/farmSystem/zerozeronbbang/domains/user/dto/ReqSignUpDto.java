50e9b84df93f88aa305616aa5fd10f99afcd9d39
thisishwan2 <feel000617@gmail.com>
2023-07-21 03:29:53 +0900

    [FIX] DTO 수정

--- src/main/java/farmSystem/zerozeronbbang/domains/user/dto/ReqSignUpDto.java
+++ src/main/java/farmSystem/zerozeronbbang/domains/user/dto/ReqSignUpDto.java
@@ -1,16 +1,27 @@
 package farmSystem.zerozeronbbang.domains.user.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
-@Data
+import javax.validation.constraints.NotBlank;
+
+@NoArgsConstructor
+@Getter
 @AllArgsConstructor
+@Builder
 public class ReqSignUpDto {
+
+    @NotBlank(message = "이메일은 필수입니다.")
     private String email;
+    @NotBlank(message = "비밀번호는 필수입니다.")
     private String password;
+    @NotBlank(message = "이름은 필수입니다.")
     private String name;
+    @NotBlank(message = "휴대폰 번호는 필수입니다.")
     private String phone;
+    @NotBlank(message = "우편번호는 필수입니다.")
     private String address1;
+    @NotBlank(message = "주소는 필수입니다.")
     private String address2;
+    @NotBlank(message = "상세주소는 필수입니다.")
     private String address3;
 }
---
+++ src/main/java/farmSystem/zerozeronbbang/domains/user/dto/ResSignUpDto.java
@@ -0,0 +1,18 @@
+package farmSystem.zerozeronbbang.domains.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class ResSignUpDto {
+
+    private Long id;
+    private String email;
+    private String name;
+
+}
