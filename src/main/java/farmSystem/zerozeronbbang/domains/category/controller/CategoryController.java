1cedab980ba53a1273f1e5b60dece9de596bec25
thisishwan2 <feel000617@gmail.com>
2023-07-21 03:28:08 +0900

    [FIX] 요청 응답 수정

--- src/main/java/farmSystem/zerozeronbbang/domains/category/controller/CategoryController.java
+++ src/main/java/farmSystem/zerozeronbbang/domains/category/controller/CategoryController.java
@@ -2,15 +2,12 @@ package farmSystem.zerozeronbbang.domains.category.controller;
 
 import farmSystem.zerozeronbbang.domains.category.dto.ResFindCategoriseDto;
 import farmSystem.zerozeronbbang.domains.category.service.impl.CategoryServiceImpl;
-import farmSystem.zerozeronbbang.global.enums.ResCodeEnum;
-import farmSystem.zerozeronbbang.response.ResponseDto;
-import farmSystem.zerozeronbbang.response.ResponseUtil;
+import farmSystem.zerozeronbbang.response.CustomResponseEntity;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,8 +21,8 @@ public class CategoryController {
 
     @Operation(summary = "category select", description = "메인 페이지 category select 컨트롤러")
     @GetMapping(value = "/categories")
-    public ResponseDto<?> findCategories(@AuthenticationPrincipal final String email){
+    public CustomResponseEntity<List<ResFindCategoriseDto>> findCategories(@AuthenticationPrincipal final String email){
         List<ResFindCategoriseDto> categories = categoryService.findCategories();
-        return ResponseUtil.SUCCESS(ResCodeEnum.CATEGORY_LOAD_SUCCESS.getMessage(), categories);
+        return CustomResponseEntity.success(categories);
     }
 }
--- src/main/java/farmSystem/zerozeronbbang/domains/menu/controller/MenuController.java
+++ src/main/java/farmSystem/zerozeronbbang/domains/menu/controller/MenuController.java
@@ -3,13 +3,12 @@ package farmSystem.zerozeronbbang.domains.menu.controller;
 import farmSystem.zerozeronbbang.domains.menu.dto.ReqFindMenuDto;
 import farmSystem.zerozeronbbang.domains.menu.dto.ResFindMenuDto;
 import farmSystem.zerozeronbbang.domains.menu.service.impl.MenuServiceImpl;
-import farmSystem.zerozeronbbang.global.enums.ResCodeEnum;
-import farmSystem.zerozeronbbang.response.ResponseDto;
-import farmSystem.zerozeronbbang.response.ResponseUtil;
+import farmSystem.zerozeronbbang.response.CustomResponseEntity;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +22,8 @@ public class MenuController {
     private final MenuServiceImpl menuService;
 
     @Operation(summary = "Menu select", description = "주문 작성 메뉴 선택")
-    @GetMapping(value = "/menus")
-    public ResponseDto<List<ResFindMenuDto>> findMenus(@RequestBody ReqFindMenuDto reqFindMenuDto){
-        return ResponseUtil.SUCCESS(ResCodeEnum.MENU_LOAD_SUCCESS.getMessage(), menuService.findMenus(reqFindMenuDto));
+    @GetMapping(value = "/{foodstoreId}/menus")
+    public CustomResponseEntity<List<ResFindMenuDto>> findMenus(@PathVariable Long foodstoreId){
+        return CustomResponseEntity.success(menuService.findMenus(foodstoreId));
     }
 }
