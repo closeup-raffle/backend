4d93391b6dbb3855d5bdd3c19de3ad626bef05df
thisishwan2 <feel000617@gmail.com>
2023-07-24 18:26:10 +0900

    [FIX] 단어 변경

--- src/main/java/farmSystem/zerozeronbbang/domains/menu/controller/MenuController.java
+++ src/main/java/farmSystem/zerozeronbbang/domains/menu/controller/MenuController.java
@@ -1,6 +1,5 @@
 package farmSystem.zerozeronbbang.domains.menu.controller;
 
-import farmSystem.zerozeronbbang.domains.menu.dto.ReqFindMenuDto;
 import farmSystem.zerozeronbbang.domains.menu.dto.ResFindMenuDto;
 import farmSystem.zerozeronbbang.domains.menu.service.impl.MenuServiceImpl;
 import farmSystem.zerozeronbbang.response.CustomResponseEntity;
@@ -9,7 +8,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,9 +19,9 @@ public class MenuController {
 
     private final MenuServiceImpl menuService;
 
-    @Operation(summary = "Menu select", description = "주문 작성 메뉴 선택")
-    @GetMapping(value = "/{foodstoreId}/menus")
-    public CustomResponseEntity<List<ResFindMenuDto>> findMenus(@PathVariable Long foodstoreId){
-        return CustomResponseEntity.success(menuService.findMenus(foodstoreId));
+    @Operation(summary = "Menu select", description = "음식점 메뉴 조회")
+    @GetMapping(value = "/menus/{food-store-id}")
+    public CustomResponseEntity<List<ResFindMenuDto>> findMenus(@PathVariable(name = "food-store-id") Long foodStoreId){
+        return CustomResponseEntity.success(menuService.findMenus(foodStoreId));
     }
 }
