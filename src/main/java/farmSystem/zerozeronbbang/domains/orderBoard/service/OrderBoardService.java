756b37d041345adfa7535218330a964d97136bf5
thisishwan2 <feel000617@gmail.com>
2023-07-24 18:27:43 +0900

    [FEAT] 메소드 추가

--- src/main/java/farmSystem/zerozeronbbang/domains/orderBoard/service/OrderBoardService.java
+++ src/main/java/farmSystem/zerozeronbbang/domains/orderBoard/service/OrderBoardService.java
@@ -1,6 +1,7 @@
 package farmSystem.zerozeronbbang.domains.orderBoard.service;
 
 import farmSystem.zerozeronbbang.domains.orderBoard.dto.ReqWriteOrderBoardDto;
+import farmSystem.zerozeronbbang.domains.orderBoard.dto.ResFindOrderBoardDto;
 import farmSystem.zerozeronbbang.domains.orderBoard.dto.ResFindOrderBoardsDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,4 +11,6 @@ public interface OrderBoardService {
     Page<ResFindOrderBoardsDto> findOrderBoards(Pageable pageable);
 
     ReqWriteOrderBoardDto writeOrderBoard(String email, ReqWriteOrderBoardDto reqWriteOrderBoardDto);
+
+    ResFindOrderBoardDto findOrderBoard(Long orderBoardId);
 }
