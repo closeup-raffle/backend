a76fdeb88bd17a24c9cb54a9678abf357435eb66
thisishwan2 <feel000617@gmail.com>
2023-07-24 18:29:19 +0900

    [FEAT] 응답 수정 및 카카오 로그인 추가

--- src/main/java/farmSystem/zerozeronbbang/domains/user/controller/UserController.java
+++ src/main/java/farmSystem/zerozeronbbang/domains/user/controller/UserController.java
@@ -1,16 +1,17 @@
 package farmSystem.zerozeronbbang.domains.user.controller;
 
-import farmSystem.zerozeronbbang.domains.user.dto.ReqLoginDto;
-import farmSystem.zerozeronbbang.domains.user.dto.ReqSignUpDto;
-import farmSystem.zerozeronbbang.domains.user.dto.ResLoginDto;
-import farmSystem.zerozeronbbang.domains.user.dto.ResSignUpDto;
-import farmSystem.zerozeronbbang.domains.user.dto.ResOauthDto;
+import farmSystem.zerozeronbbang.domains.user.dto.request.ReqLoginDto;
+import farmSystem.zerozeronbbang.domains.user.dto.request.ReqSignUpDto;
+import farmSystem.zerozeronbbang.domains.user.dto.response.ResLoginDto;
+import farmSystem.zerozeronbbang.domains.user.dto.response.ResSignUpDto;
+import farmSystem.zerozeronbbang.domains.user.dto.response.ResOauthDto;
 import farmSystem.zerozeronbbang.domains.user.service.Impl.OauthKaKaoServiceImpl;
 import farmSystem.zerozeronbbang.domains.user.service.Impl.UserServiceImpl;
 import farmSystem.zerozeronbbang.response.CustomResponseEntity;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,7 +19,8 @@ import javax.validation.Valid;
 @Tag(name = "User", description = "유저 인증 관련 API")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
+@Slf4j
 public class UserController {
 
     private final UserServiceImpl userService;
@@ -26,17 +28,18 @@ public class UserController {
     private final OauthKaKaoServiceImpl oauthKaKaoService;
 
     // GET
-    @Operation(summary = "user 조회 TEST", description = "user login 컨트롤러")
-    @GetMapping(value = "/test")
-    public ResponseDto<?> test(@AuthenticationPrincipal User userAccount, String email) {
-        return ResponseUtil.SUCCESS(ResCodeEnum.USER_FIND_SUCCESS.getMessage(), userService.findUser(email));
-    }
+//    @Operation(summary = "user 조회 TEST", description = "user login 컨트롤러")
+//    @GetMapping(value = "/test")
+//    public CustomResponseEntity<ResponseDto> test(@AuthenticationPrincipal User userAccount, String email) {
+//        return ResponseUtil.SUCCESS(ResCodeEnum.USER_FIND_SUCCESS.getMessage(), userService.findUser(email));
+//    }
 
     @ResponseBody
     @GetMapping("/login/kakao")
-    public ResOauthDto kakaoCalllback(@RequestParam String code) {
-        String accesstoken = oauthKaKaoService.getKakaoAccessToken(code);
-        return oauthKaKaoService.getKakaoUserInfo(accesstoken);
+    public CustomResponseEntity<ResOauthDto> kakaoCalllback(@RequestParam String code) {
+        String accessToken = oauthKaKaoService.getKakaoAccessToken(code);
+        log.info(accessToken);
+        return CustomResponseEntity.success(oauthKaKaoService.getKakaoUserInfo(accessToken));
     }
 
     // POST
