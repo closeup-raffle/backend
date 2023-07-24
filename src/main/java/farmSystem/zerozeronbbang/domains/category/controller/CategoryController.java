96d5befd1a0d93e02679080e254899bbe909f136
thisishwan2 <feel000617@gmail.com>
2023-07-24 18:24:51 +0900

    [FIX] 오타 수정

--- src/main/java/farmSystem/zerozeronbbang/domains/category/controller/CategoryController.java
+++ src/main/java/farmSystem/zerozeronbbang/domains/category/controller/CategoryController.java
@@ -19,9 +19,9 @@ public class CategoryController {
 
     private final CategoryServiceImpl categoryService;
 
-    @Operation(summary = "category select", description = "메인 페이지 category select 컨트롤러")
+    @Operation(summary = "category select", description = "주문 공고 확인시 category 전체 조회 컨트롤러")
     @GetMapping(value = "/categories")
-    public CustomResponseEntity<List<ResFindCategoriseDto>> findCategories(@AuthenticationPrincipal final String email){
+    public CustomResponseEntity<List<ResFindCategoriseDto>> findCategories(){
         List<ResFindCategoriseDto> categories = categoryService.findCategories();
         return CustomResponseEntity.success(categories);
     }
