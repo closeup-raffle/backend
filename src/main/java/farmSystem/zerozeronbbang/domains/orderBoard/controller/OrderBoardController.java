599a4d6daa770304c33f15904f5994987921ac0d
thisishwan2 <feel000617@gmail.com>
2023-07-24 18:26:46 +0900

    [FEAT] 주문 공고 단건 조회 추가

--- src/main/java/farmSystem/zerozeronbbang/domains/orderBoard/controller/OrderBoardController.java
+++ src/main/java/farmSystem/zerozeronbbang/domains/orderBoard/controller/OrderBoardController.java
@@ -1,6 +1,7 @@
 package farmSystem.zerozeronbbang.domains.orderBoard.controller;
 
 import farmSystem.zerozeronbbang.domains.orderBoard.dto.ReqWriteOrderBoardDto;
+import farmSystem.zerozeronbbang.domains.orderBoard.dto.ResFindOrderBoardDto;
 import farmSystem.zerozeronbbang.domains.orderBoard.dto.ResFindOrderBoardsDto;
 import farmSystem.zerozeronbbang.domains.orderBoard.service.OrderBoardServiceImpl;
 import farmSystem.zerozeronbbang.response.CustomResponseEntity;
@@ -12,10 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "OrderBoard", description = "주문 공고글 api")
 @RequiredArgsConstructor
@@ -26,16 +24,22 @@ public class OrderBoardController {
 
     @Operation(summary = "orderBoard list", description = "orderBoard 리스트 조회 컨트롤러")
     @GetMapping(value = "/order-boards")
-    public CustomResponseEntity<Page<ResFindOrderBoardsDto>> getOrderBoards(@AuthenticationPrincipal final String email, @PageableDefault(size=10, sort = "createdAt", direction = Sort.Direction.DESC)Pageable pageable){
+    public CustomResponseEntity<Page<ResFindOrderBoardsDto>> getOrderBoards(@PageableDefault(size=10, sort = "createdAt", direction = Sort.Direction.DESC)Pageable pageable){
         Page<ResFindOrderBoardsDto> orderBoards = orderBoardService.findOrderBoards(pageable);
         return CustomResponseEntity.success(orderBoards);
     }
 
-    //@Authen~~에서 email 가져오는 부분이 안됨. ㄴervice에서 user 조횝 부분 null값 뜸
-    @Operation(summary = "orderBoard list", description = "orderBoard 리스트 조회 컨트롤러")
-    @PostMapping(value = "/order-board")
+    //@Authen~~에서 userdetails가 정의되어 잇지 않아서 정보를 가져오지 못함.
+    @Operation(summary = "orderBoard list", description = "orderBoard 작성 컨트롤러")
+    @PostMapping(value = "/order-boards")
     public CustomResponseEntity<?> writeOrderBoard(@AuthenticationPrincipal final String email, @RequestBody ReqWriteOrderBoardDto reqWriteOrderBoardDto){
         return CustomResponseEntity.success(orderBoardService.writeOrderBoard(email, reqWriteOrderBoardDto));
+    }
+
+    @Operation(summary = "orderBoard list", description = "orderBoard 상세 조회(단건) 컨트롤러")
+    @GetMapping(value = "/order-boards/{order-board-id}")
+    public CustomResponseEntity<ResFindOrderBoardDto> getOrderBoard(@PathVariable(name = "order-board-id") Long orderBoardId){
+        return CustomResponseEntity.success(orderBoardService.findOrderBoard(orderBoardId));
     }
 
 
